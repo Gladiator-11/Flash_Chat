3de7d2d7dc0624c8414aa120e476e19558c762d3
Gladiator-11 <ravalaakash11@gmail.com>
2021-12-22 21:51:08 +0530

    final

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flash_chat/components/round_button.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id = 'login_screen';
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 200.0,
+              child: Image.asset('images/logo.png'),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration:
+                  kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter Your Password'),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundButton(
+              colour: Colors.blue,
+              title: 'LogIn',
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
